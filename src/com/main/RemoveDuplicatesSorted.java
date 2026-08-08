edf5fedf5cf3fda97e0cc6bec4e094af7a5027bd
Abishek Kumar <abishekkumar2425@gmail.com>
2026-08-08 23:35:21 +0530

    Changes

--- src/com/main/RemoveDuplicatesSorted.java
+++ src/com/main/RemoveDuplicatesSorted.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RemoveDuplicatesSorted {
 
 	public static void main(String[] args) {
-		int arr[] = {0,0,1,1,1,2,2,3,4,4,4,5,5,6};
+		int arr[] = {0,0,1,1,1,,2,3,4,4,4,5,5,6};
 		int i = 0, j = 1, k = 1;
         while(j < arr.length){
         	if(arr[i] == arr[j]) {
