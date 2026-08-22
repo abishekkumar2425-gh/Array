c09257433b7c07f29fab3fe1d03f324f1b87a967
Abishek Kumar <abishekkumar2425@gmail.com>
2026-08-22 22:21:07 +0530

    Container With Most Water

---
+++ src/com/main/ContainerMostWater.java
@@ -0,0 +1,28 @@
+package com.main;
+public class ContainerMostWater {
+    public static int storeWater(int arr[]) {
+    	int maxWater = 0;
+    	int le = 0;
+    	int ri = arr.length-1;
+    	while(le < ri) {
+    		int ht = Math.min(arr[le],arr[ri]);
+    		int wd = ri - le;
+    		int currWater = ht * wd;
+    		maxWater = Math.max(maxWater, currWater);
+    		if(arr[le]<arr[ri]) {
+    			le++;
+    		}
+    		else {
+    			ri--;
+    		}
+    	}
+    	return maxWater;
+    }
+	public static void main(String[] args) {
+		int[] arr = {1,8,6,2,5,4,8,3,7};
+
+		
+        System.out.println(storeWater(arr));
+	}
+
+}
