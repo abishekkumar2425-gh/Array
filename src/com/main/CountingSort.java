6ce3893b6b0366f28ccc5168ee19c3fbcdb702fe
Abishek Kumar <abishekkumar2425@gmail.com>
2026-08-31 23:41:55 +0530

    Updates

--- src/com/main/CountingSort.java
+++ src/com/main/CountingSort.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CountingSort {
 
 	public static void main(String[] args) {
-		int arr[] = {6,4,4,2,1,7,2,1,3};
+		int arr[] = {6,4,2,1,0,2,1,3};
 		System.out.println("Unsorted Array : " + Arrays.toString(arr)); 
 		int largest = Integer.MIN_VALUE;
 		for(int i = 0; i < arr.length ; i++) {
