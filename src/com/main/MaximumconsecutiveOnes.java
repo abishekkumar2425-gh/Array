1441d18abae2e0965ffd6dea2887b28858dbfc3d
Abishek Kumar <abishekkumar2425@gmail.com>
2026-09-03 23:10:36 +0530

    Changes

--- src/com/main/MaximumconsecutiveOnes.java
+++ src/com/main/MaximumconsecutiveOnes.java
@@ -4,7 +4,7 @@ package com.main;
 public class MaximumconsecutiveOnes {
 
 	public static void main(String[] args) {
-		int arr[] = {1,1,0,0,1,0,1,1,1,0,1,1};
+		int arr[] = {1,1,0,0,1,0,1,1,0,1,1};
 		int c = 0, maxc = 0;
         for( int i = 0 ; i < arr.length ; i++){
         	if(arr[i]==1) {
