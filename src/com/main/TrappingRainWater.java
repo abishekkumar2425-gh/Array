89f14698037e9649c460bcdc160360667a60b2c0
Abishek Kumar <abishekkumar2425@gmail.com>
2026-09-11 23:55:39 +0530

    Updates

--- src/com/main/TrappingRainWater.java
+++ src/com/main/TrappingRainWater.java
@@ -19,7 +19,7 @@ public class TrappingRainWater {
     	return ans;
     }
 	public static void main(String[] args) {
-	   int arr[] = {5,1,1,1,1,5};
+	   int arr[] = {5,1,2,1,1,5};
 	   System.out.println(trapping(arr));
 
 	}
