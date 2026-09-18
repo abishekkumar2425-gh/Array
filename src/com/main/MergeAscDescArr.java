d94b09920098344b7d145b19896cd4323a03b68f
Abishek Kumar <abishekkumar2425@gmail.com>
2026-09-18 23:59:42 +0530

    Changes

--- src/com/main/MergeAscDescArr.java
+++ src/com/main/MergeAscDescArr.java
@@ -18,7 +18,7 @@ public class MergeAscDescArr {
 				i++;
 			}
 			else if(arr1[i]<arr2[j]) {
-				result[] = arr1[i];
+				result[k] = arr1[i];
 				i++;
 			}
 			else {
@@ -33,7 +33,7 @@ public class MergeAscDescArr {
 	}
 
 	public static void main(String[] args) {
-		int arr1[] = {2,5,7,12,14};
+		int arr1[] = {2,5,7,12};
 		int arr2[] = {15,13,9,6,3,1};
 		int res[]=merged(arr1,arr2);
 		System.out.println(Arrays.toString(res));
